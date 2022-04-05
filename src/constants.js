7763dea0254b76c23a34936ceae378a1b19272d3
Dzunnurain Achmad Azhar <dzunnurainaaz@gmail.com>
2022-04-05 12:31:55 +0700

    ready to deploy

---
+++ src/constants.js
@@ -0,0 +1,14 @@
+const CONTRACT_ADDRESS = '0x8777742b8e4b4320e7EB89C488471A3577cc31c7';
+/*
+ * Add this method and make sure to export it on the bottom!
+ */
+const transformCharacterData = (characterData) => {
+    return {
+      name: characterData.name,
+      imageURI: characterData.imageURI,
+      hp: characterData.hp.toNumber(),
+      maxHp: characterData.maxHp.toNumber(),
+      attackDamage: characterData.attackDamage.toNumber(),
+    };
+  };
+export { CONTRACT_ADDRESS, transformCharacterData };
